a70dce43b94dd2f3afb66759fb80ca73d6c534e6
RAJANI9 <83950602+RAJANI9@users.noreply.github.com>
2022-01-31 20:31:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
-customhost = "web.cc1dflwlgcer.ap-south-1.rds.amazonaws.com"
+customhost = "mydb.cc1dflwlgcer.ap-south-1.rds.amazonaws.com"
 customuser = "admin"
 custompass = "mypassword"
 customdb = "employee"
-custombucket = "webs32022"
+custombucket = "myemps32022"
 customregion = "ap-south-1"
